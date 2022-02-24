344773310d8f6ae69af7289525addf934f07d58b
Aleja Martinez <martinezmayra605@gmail.com>
2022-02-23 23:39:54 -0500

    CAmbio en las rutas de maping del controller

--- src/main/java/co/com/sofka/crud/Service/TodoService.java
+++ src/main/java/co/com/sofka/crud/Service/TodoService.java
@@ -11,18 +11,22 @@ public class TodoService {
     @Autowired
     private TodoRepository repository;
 
+    //buscar todas las entidades
     public Iterable<Todo> list(){
         return repository.findAll();
     }
 
+    //guardar la entidad
     public Todo save(Todo todo){
         return repository.save(todo);
     }
 
+    //borrar por id
     public void delete(Long id){
         repository.delete(get(id));
     }
 
+    //buscar por id
     public Todo get(Long id){
          return repository.findById(id).orElseThrow();
     }
